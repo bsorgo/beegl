fa8edbbdbfabd2d1321feb79255af3cd3713a412
Bostjan Sorgo <bostjan.sorgo@gmail.com>
2019-08-05 13:32:21 +0200

    Release management

---
+++ esp32/beegl_broker/post_build.py
@@ -0,0 +1,17 @@
+ry:
+    import configparser
+except ImportError:
+    import ConfigParser as configparser
+
+Import("env")
+print(env.Dump())
+
+config = configparser.ConfigParser()
+config.read("platformio.ini")
+
+progname_prefix = config.get("common","progname_prefix")
+variant = env['PIOENV']
+ver = config.get("common","ver")
+
+
+env.Replace(PROGNAME="%s%s_%s" % (progname_prefix, variant, ver))
